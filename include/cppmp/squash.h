7839f9d54a2ebc49c6332dd3e038c6d2af4b96fc
mori0091 <mori-d@qc4.so-net.ne.jp>
2023-11-12 17:05:52 +0900

    add LICENSE

--- include/cppmp/squash.h
+++ include/cppmp/squash.h
@@ -1,3 +1,13 @@
+// -*- coding: utf-8-unix -*-
+
+// Copyright (c) 2023 Daishi Mori (mori0091)
+//
+// This software is released under the MIT License.
+// See https://github.com/mori0091/cppmp/blob/main/LICENSE
+//
+// GitHub cppmp project
+// https://github.com/mori0091/cppmp
+
 #ifndef CPPMP_SQUASH_H_
 #define CPPMP_SQUASH_H_
 
